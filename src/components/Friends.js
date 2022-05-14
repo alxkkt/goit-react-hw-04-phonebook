024fadb329afc0c3c594871d1b5a0979e8597e45
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-14 15:39:11 +0200

    added a few test files

---
+++ src/components/Friends.js
@@ -0,0 +1,10 @@
+function Friends() {
+  return (
+    <ul>
+      <li>Alpha</li>
+      <li>Omega</li>
+    </ul>
+  );
+}
+
+export default Friends;
---
+++ src/components/Profile.js
@@ -0,0 +1,5 @@
+function Profile(props) {
+  return <h1>This is a test</h1>;
+}
+
+export default Profile;
