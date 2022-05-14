795ac9fc6736d64c95e921a35286eebffdc4f85f
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-14 15:38:52 +0200

    tested some features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import Profile from "./Profile";
+import Friends from "./Friends";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile />
+      <Friends />
     </div>
   );
 };
