4b587453865736e6590296ddb8d519adf402769b
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-19 17:30:56 +0200

    deleted all extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,8 @@
 import { Component } from 'react';
-import Section from 'components/Section';
-import Statistics from 'components/Statistics';
-import FeedbackOptions from 'components/FeedbackOptions';
-import Notification from 'components/Notification';
 
 class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  onLeaveFeedback = property => {
-    this.setState(prevState => {
-      return {
-        [property]: (prevState[property] += 1),
-      };
-    });
-  };
-  countTotalFeedback() {
-    const values = Object.values(this.state);
-
-    return values.reduce(
-      (prevValue, currentValue) => (prevValue += currentValue),
-      0,
-    );
-  }
-  countPositiveFeedbackPercentage() {
-    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0);
-  }
   render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
-        <Section title={'Please, leave feedback'}>
-          <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback() ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
+    return <div>Nothing to see here</div>;
   }
 }
 
--- src/randomColor.js
+++
@@ -1,3 +0,0 @@
-export default function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
